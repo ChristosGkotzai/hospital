b22a800e36fde4529c2164921a30342a1897c30b
Godxhaj7 <xristosgko7@gmail.com>
2025-10-20 14:21:49 +0300

    doctorRepo-Interface

---
+++ Repos/Interfaces/IDoctorRepo.cs
@@ -0,0 +1,13 @@
+using hospital.Models;
+
+namespace hospital.Repos.Interface
+{
+    public interface IDoctorRepo
+    {
+        Task<IEnumerable<Doctor>> GetAll();
+        Task<Doctor?> GetById(int id);
+        Task<Doctor> Add(Doctor doctor);
+        Task Update(Doctor doctor);
+        Task  Delete(int id);
+    }
+}
